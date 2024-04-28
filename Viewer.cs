3fb17f4d8aaac853af63d77cf91b13c1ab8fa339
romeoliveirasantos <rjob.oliveira@gmail.com>
2024-04-28 12:51:07 -0300

    refac

--- Viewer.cs
+++ Viewer.cs
@@ -21,33 +21,33 @@ namespace EditorHtml
     }
     public static void Replace(string text)
     {
-     var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
-     var words = text.Split(" ");
+      var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+      var words = text.Split(" ");
 
-     for(var i =0; i < words.Length; i++)
-     {
-      if(strong.IsMatch(words[i]))
+      for (var i = 0; i < words.Length; i++)
       {
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.Write(
-          words[i].Substring(
-            words[i].IndexOf('>') + 1, 
-            (
-              words[i].LastIndexOf('<') -1 - 
-              words[i].IndexOf('>')            
+        if (strong.IsMatch(words[i]))
+        {
+          Console.ForegroundColor = ConsoleColor.Blue;
+          Console.Write(
+            words[i].Substring(
+              words[i].IndexOf('>') + 1,
+              (
+                words[i].LastIndexOf('<') - 1 -
+                words[i].IndexOf('>')
+              )
             )
-          )
-        );
-        Console.Write(" ");
-      }
-      else
-      {
-        Console.ForegroundColor = ConsoleColor.Black;
-        Console.Write(words[i]);
-        Console.Write(" ");
+          );
+          Console.Write(" ");
+        }
+        else
+        {
+          Console.ForegroundColor = ConsoleColor.Black;
+          Console.Write(words[i]);
+          Console.Write(" ");
 
+        }
       }
-     }
     }
   }
 }
